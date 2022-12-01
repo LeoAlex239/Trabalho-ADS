68e3d6eb79f6fccacf2d022fa8a8aca9e1a7be62
LeoAlex239 <97555704+LeoAlex239@users.noreply.github.com>
2022-12-01 17:02:33 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class teste2 {
+  
+  public static void main(String[] args) {
+		calculadora calculadora = new calculadora();
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int opcao = -1;
+
+		
+		double N1, N2;
+		while (true) {
+			System.out.print("Digite uma opção (1-soma, 2-subtracao, 3-multiplicacao, 4-divisao, 5-exponenciação, 6-raiz, 7-teto, 8-piso, 0-sair): ");
+			opcao = scanner.nextInt();
+			
+			if(opcao == 0)
+				break;
+
+        if(opcao >8)
+        break;
+        
+		
+			switch (opcao) {
+			case 1:{
+				System.out.print("Digite o primeiro numero: ");
+			N1 = scanner.nextDouble();
+			System.out.print("Digite o segundo numero: ");
+			N2 = scanner.nextDouble();
+
+				System.out.println(calculadora.soma(N1, N2));
+				break;
+			}
+			case 2:{
+				System.out.print("Digite o primeiro numero: ");
+			N1 = scanner.nextDouble();
+			System.out.print("Digite o segundo numero: ");
+			N2 = scanner.nextDouble();
+
+				System.out.println(calculadora.sub(N1, N2));
+				break;
+			}
+			case 3:{
+				System.out.print("Digite o primeiro numero: ");
+			N1 = scanner.nextDouble();
+			System.out.print("Digite o segundo numero: ");
+			N2 = scanner.nextDouble();
+
+				System.out.println(calculadora.multi(N1, N2));
+				break;
+			}
+      
+			}
+		}
+		
+		scanner.close();
+	}
+
+}
